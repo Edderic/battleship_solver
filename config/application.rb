6ea10daec2173f5df878ed9a3b3da1eb892d768d
Edderic Ugaddan <edderic@gmail.com>
2015-09-27 17:34:02 -0400

    remove stackprof from application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module BattleshipSolver
   class Application < Rails::Application
-    config.middleware.use(StackProf::Middleware, enabled:true, mode: :wall, interval: 1000, save_every: 5)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
